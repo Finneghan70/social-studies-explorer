4f4baf61b1f6e26db8192b555ffcbc33846e8802
Finneghan70 <dyanwhite@hotmail.com>
2026-09-22 18:07:20 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const siteConfig = {
+
+    title: "BC Social Studies Explorer",
+
+    subtitle:
+        "Investigate. Discover. Think Like a Historian.",
+
+    background:
+        "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+
+    startButton:
+        "Choose Your Mission"
+
+};
+``
